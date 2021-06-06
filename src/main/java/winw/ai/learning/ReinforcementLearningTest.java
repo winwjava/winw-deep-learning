60782bb769003919fb950c1af7e9bba8cdc0385d
winw <winwjava@sina.com>
2021-06-06 13:06:09 +0800

    Face Recognition

--- src/main/java/winw/ai/learning/ReinforcementLearningTest.java
+++ src/main/java/winw/ai/learning/ReinforcementLearningTest.java
@@ -3,11 +3,16 @@ package winw.ai.learning;
 /**
  * 强化学习，主要是主体Agent根据处境State，做出行为Action，并且最大化奖励Reward的过程。
  * <ul>
- * <li>1、Agent根据处境StateA做出Action，并预期一个Reward。
+ * <li>1、Agent根据处境State做出Action，并预期一个Reward。
  * <li>2、Agent的Action影响到环境。
  * <li>3、Agent识别到环境的Reward。
  * <li>4、Agent根据预测的Reward和实际的Reward改进对StateA的Action。
  * </ul>
+ * 
+ * <p>
+ * 经典非条件反射：是StateX 与另外一个RewardY之间建立联系，并强化学习。
+ * <p>
+ * 操作性条件反射：是ActionX 与另外一个RewardY之间建立联系，并强化学习。
  * 
  * @author winw
  *
---
+++ src/main/java/winw/ai/recognition/FaceRecognition.java
@@ -0,0 +1,5 @@
+package winw.ai.recognition;
+
+public class FaceRecognition {
+
+}
